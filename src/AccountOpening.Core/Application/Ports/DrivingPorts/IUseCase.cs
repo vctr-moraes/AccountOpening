d3c50d0f4c8dbe2b02afa1774e37a88d771f0252
Victor Moraes <victor.moraes@outlook.com.br>
2026-08-26 14:03:49 -0300

    Adiciona padrão de casos de uso com IUseCase e UseCase
    Foram criados os arquivos IUseCase.cs e UseCase.cs. IUseCase define uma interface genérica para execução assíncrona de casos de uso com DTOs. UseCase implementa uma classe abstrata que fornece uma implementação padrão para TryExecuteAsync, delegando a execução para o método abstrato ExecuteAsync. Essas adições promovem reutilização e padronização na criação de casos de uso na aplicação.

---
+++ src/AccountOpening.Core/Application/Ports/DrivingPorts/IUseCase.cs
@@ -0,0 +1,9 @@
+﻿using AccountOpening.Core.Application.DTOs;
+
+namespace AccountOpening.Core.Application.Ports.DrivingPorts
+{
+    public interface IUseCase<TInput, TOutput> where TInput : Dto where TOutput : Dto
+    {
+        Task<TOutput> TryExecuteAsync(TInput input);
+    }
+}
---
+++ src/AccountOpening.Core/Application/UseCases/UseCase.cs
@@ -0,0 +1,16 @@
+﻿using AccountOpening.Core.Application.DTOs;
+using AccountOpening.Core.Application.Ports.DrivingPorts;
+
+namespace AccountOpening.Core.Application.UseCases
+{
+    public abstract class UseCase<TInput, TOutput> : IUseCase<TInput, TOutput> where TInput : Dto where TOutput : Dto
+    {
+        public async Task<TOutput> TryExecuteAsync(TInput input)
+        {
+            // TODO: add validations for the input dto here.
+            return await ExecuteAsync(input);
+        }
+
+        protected abstract Task<TOutput> ExecuteAsync(TInput input);
+    }
+}
